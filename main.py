413803f58d4acc92719cf49904bf2487f0340e29
xnaut <xnaut@users.noreply.github.com>
2017-09-20 04:00:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 import logging
 from random import randint
+from owner_commands import owner_commands
 
 client = discord.Client()
 
@@ -10,6 +11,14 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+# Command prefixes
+pf = '.'
+pf_addquote = pf + 'addquote'
+pf_randint = pf +'randint'
+pf_repeat = pf + 'repeat'
+pf_help = pf + 'help'
+pf_quote = pf + 'q'
 
 @client.event
 async def on_ready():
@@ -35,26 +44,27 @@ async def on_message(message):
     # Display all messages in terminal
     print('[' + str(message.channel) + '] ' + str(message.author) + ': ' +  str(message.content))
 
-    # Stops bot from acting upon itself.
+    async def quote_embed(author, channel):
+        em = discord.Embed(description=message.content, color=0x6DC066)
+        em.set_author(name=author, icon_url=author.avatar_url)
+        await client.send_message(channel, embed=em)
+
+    # Stops the bot from acting upon itself
     if message.author == client.user:
         return
 
-    def quote_embed():
-        em = discord.Embed(description=message.content, color=0x6DC066)
-        em.set_author(name=message.author, icon_url=message.author.avatar_url)
-
     # Chooses random number between 1 & 1000
-    if message.content.startswith('$randint'):
+    if message.content.startswith(pf_randint):
         await client.send_message(message.channel, randint(1, 1000))
 
     # Repeats anything the user says
-    if message.content.startswith("$repeat"):
+    if message.content.startswith(pf_repeat):
         await client.delete_message(message)
         await client.send_message(message.channel, message.content.replace("$repeat", ""))
 
     # Adds quotes to quote file
-    if message.content.startswith('$addquote'):
-        quote = message.content.replace('$addquote ', '`')
+    if message.content.startswith(pf_addquote):
+        quote = message.content.replace(pf_addquote + ' ', '`')
 
         f = open('quotes.txt', 'a')
         f.write(quote + '\n')
@@ -63,44 +73,27 @@ async def on_message(message):
         await client.send_message(message.channel, 'Quote added')
 
     # Help
-    if message.content.startswith('$help'):
+    if message.content.startswith(pf_help):
         f = open('help.txt', 'r')
         em = discord.Embed(title='JBot Help', description=f.read(), color=0x6DC066)
         await client.send_message(message.channel, embed=em)
         f.close()
 
     # Quote message
-    if message.content.startswith('$q'):
-        tmp = message.content.replace('.q', '').strip().lower()
-        await client.delete_message(message)
+    if message.content.startswith(pf_quote):
+        tmp = message.content.replace(pf_quote, '').strip().lower()
 
-        async for message in client.logs_from(message.channel, limit=50):
-            if message.content.lower().startswith(tmp):
-                em = discord.Embed(description=message.content, color=0x6DC066)
-                em.set_author(name=message.author, icon_url=message.author.avatar_url)
-                await client.send_message(message.channel, embed=em)
+        async for message in client.logs_from(message.channel, limit=1000):
+
+            message_sent = False
+
+            if tmp in message.content.lower() and message.content.startswith(pf_quote) == False:
+                await quote_embed(message.author, message.channel)
+                message_sent = True
                 return
 
-    if message.content.startswith('.test'):
-        test()
-
-
-    if message.content.startswith('$rand'):
-        random = randint(1, 10000)
-        counter = 0
-
-        async for message in client.logs_from(message.channel, limit=10000):
-
-            counter += 1
-
-            if counter == random:
-                em = discord.Embed(description=message.content, color=0x6DC066)
-                em.set_author(name=message.author, icon_url=message.author.avatar_url)
-                await client.send_message(message.channel, embed=em)
-                return
-            #else:
-            #    await client.send_message(message.channel, 'Nothing found.')
-            #    return
+        if message_sent == False:
+            await client.send_message(message.channel, 'No matches found.')
 
 # Fetches bot token from external .txt file
 f = open('token.txt', 'r')
