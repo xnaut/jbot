0ef97bc87f05c2b9f85f5000aa50a7af8bc51565
xnaut <xnaut@users.noreply.github.com>
2017-09-22 19:03:17 -0500

    Update and rename main.py to jbot.py

---
+++ jbot.py
@@ -0,0 +1,32 @@
+import asyncio
+import discord
+from commands import commands
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+# Bot commands
+@client.event
+async def on_message(message):
+    # Display all messages in terminal
+    print('[' + str(message.channel) + '] ' + str(message.author) + ': ' +  str(message.content))
+
+    # Stops the bot from acting upon itself
+    if message.author == client.user: # ADD LATER:  Stop from acting upon other bots as well.
+        return
+
+    await commands(discord, message, client)
+
+# Fetches bot token from external .txt file
+f = open('token.txt', 'r')
+token = f.read().strip()
+f.close()
+client.run(token)
+client.close()
